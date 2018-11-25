f1c77993f7f30233bf0c8306546f75605541bbc8
k32 <10274441+k32@users.noreply.github.com>
2018-11-25 19:14:19 +0100

    Sketchy last-minute demo stuff

--- src/demo.erl
+++ src/demo.erl
@@ -2,6 +2,14 @@
 
 -export([main/1]).
 
+-type stupid_list(A) :: {cons, A, stupid_list(A)}
+                      | nil
+                      .
+
+-type maybe_stupid_list(A) :: list(A)
+                            | stupid_list(A)
+                            .
+
 -include("lee_types.hrl").
 
 %% Create the configuration data model
@@ -23,29 +31,42 @@ model() ->
                            , cli_param => "bar"
                            , cli_short => "b"
                            }}
+               , list => {[value, consult]
+                         , #{ type => maybe_stupid_list(atom())
+                            , mandatory => true
+                            , oneliner => "Term to verify"
+                            , doc => "Blah blah"
+                            , file_key => term
+                            }}
                },
     {ok, Model} = lee_model:merge([ lee:base_model()
                                   , lee:base_metamodel()
                                   %%, lee_cli:metamodel()
                                   , lee_env:metamodel()
                                   , lee_map_getter:model()
+                                  , lee:type_refl([my_types], [ stupid_list/1
+                                                              , maybe_stupid_list/1
+                                                              ])
                                   , MyModel
                                   ]),
     Model.
 
 main(Args) ->
     Model = model(),
-    Config = lee_env:read(Model),
+    Config0 = lee_env:read(Model),
+    Config1 = lee_consult:read(Model, "test.eterm"),
     %%Config1 = lee_cli:read(Model, CliAttrs),
     %% Let's suppose CLI arguments should override environment variables:
-    %Config = maps:merge(Config0, Config1),
+    Config = maps:merge(Config0, Config1),
     case lee:validate(Model, Config) of
         {ok, _Warnings} ->
             {ok, Foo} = lee:get(Model, Config, [foo]),
             {ok, Bar} = lee:get(Model, Config, [bar]),
+            {ok, List} = lee:get(Model, Config, [list]),
             io:format( "foo: ~p~n"
                        "bar: ~p~n"
-                     , [Foo, Bar]
+                       "list: ~p~n"
+                     , [Foo, Bar, List]
                      );
         {error, Errors, _Warnings} ->
             io:format("Invalid config: ~p~n", [Errors]),
---
+++ src/lee_consult.erl
@@ -0,0 +1,42 @@
+-module(lee_consult).
+
+%% Sketchiness level: > 9000
+
+-export([ metamodel/0
+        , read/2
+        ]).
+
+-spec metamodel() -> lee:model_fragment().
+metamodel() ->
+    %% TODO: There's no metamodel validation yet...
+    #{}.
+
+-spec read(lee:model_fragment(), string()) -> term().
+read(Model, Filename) ->
+    Idx = lee_model:mk_metatype_index(Model),
+    CliArgs = map_sets:to_list(maps:get(consult, Idx, #{})),
+    case file:consult(Filename) of
+        {ok, Terms} ->
+            ok;
+        _ ->
+            Terms = []
+    end,
+    {_, #{ empty := Empty
+         , put   := Put
+         }, _} = lee_model:get([lee, storage], Model),
+    lists:foldl( fun(Var, Acc) ->
+                         read_val(Model, Terms, Put, Var, Acc)
+                 end
+               , Empty(Model)
+               , CliArgs
+               ).
+
+read_val(Model, Terms, Put, Var, Acc) ->
+    {_, Attrs, _} = lee_model:get(Var, Model),
+    Key = maps:get(file_key, Attrs),
+    case proplists:lookup(Key, Terms) of
+        none ->
+            Acc;
+        {_, Val} ->
+            Put(Model, Acc, Var, Val)
+    end.
